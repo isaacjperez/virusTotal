447cc165f126beade79e6cdac115311afb72e8fc
isaacjperez <73368486+isaacjperez@users.noreply.github.com>
2023-12-29 16:07:45 -0600

    Fixed issue where external to external connections were not being caputured.

--- analyzePcap.py
+++ analyzePcap.py
@@ -11,7 +11,6 @@ import ipaddress
 def analyze_pcap_data(merged_file, output_file):
     # Load the merged DataFrame with updated information
     # output of mergedSheet.py
-
     df_sheet1 = pd.read_excel(merged_file, sheet_name='Sheet1')
 
     # Define a function to check if an IP address is private
@@ -23,23 +22,23 @@ def analyze_pcap_data(merged_file, output_file):
             return False
 
     # Fill missing values in 'Last Analysis Date' with a default value (e.g., 'Unknown')
-    # If there is no lat analysis date those rows will be dropped, so it assigned Unknown.
+    # If there is no last analysis date, those rows will be dropped, so it is assigned 'Unknown'.
     df_sheet1['Last Analysis Date'] = df_sheet1['Last Analysis Date'].fillna('Unknown')
 
     # Filter rows with private IP addresses in either 'Source' or 'Destination'
     private_ip_connections = df_sheet1[
-        (df_sheet1['Source'].apply(is_private)) |
-        (df_sheet1['Destination'].apply(is_private))
-        ]
+        (~df_sheet1['Source'].apply(is_private)) |
+        (~df_sheet1['Destination'].apply(is_private))
+    ]
 
-    # Group by private IP addresses, port, protocol, and Virus Total Verdict, and count the number of connections
+    # Group by IP addresses, port, protocol, and Virus Total Verdict, and count the number of connections
     # Add or remove columns that are present in the pcap csv
     grouped_data = private_ip_connections.groupby(
         ['Source', 'Destination', 'Protocol', 'Src port', 'Dst port', 'Virus Total Verdict', 'as_owner',
          'Verdict Count', 'Last Analysis Date']).size().reset_index(name='Connection Count')
 
     # Print the analysis results with all columns from 'Sheet1'
-    print("Assets reaching out to private IP addresses:")
+    print("Assets with connections:")
     print(grouped_data)
 
     # Save the analysis results to a new Excel file
@@ -48,5 +47,5 @@ def analyze_pcap_data(merged_file, output_file):
 
     print(f"Analysis results saved to: {output_file}")
 
-# Call the analysis function
-# analyze_pcap_data()
+
+
